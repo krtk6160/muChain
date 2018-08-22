ca63fecac11d8b362c32d0196cecbc74119d6a92
krtk6160 <krtk.6160@gmail.com>
2018-08-22 21:13:14 +0530

    Implemented file upload and hash retrieval

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+//import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
+import ipfs from './utils/ipfs.js'
 
 import './css/oswald.css'
 import './css/open-sans.css'
@@ -13,7 +14,9 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      hash: null,
+      buffer: null
     }
   }
 
@@ -28,64 +31,79 @@ class App extends Component {
       })
 
       // Instantiate contract once web3 provided.
-      this.instantiateContract()
+      //this.instantiateContract()
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
+  //function written by Kartik Shah (krtk6160)
+  captureFile = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    const file = evt.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => this.convertToBuffer(reader);
+  }
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+  //function written by Kartik Shah (krtk6160)
+  convertToBuffer = async(reader) => {
+    const buffer = Buffer.from(reader.result);
+    this.setState({
+       buffer: buffer
+    });
+  }
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+  //function written by Kartik Shah (krtk6160)
+  upload = async(evt) => {
+    evt.preventDefault();
+    console.log("upload invoked");
+    await ipfs.add(this.state.buffer, (err, res) => {
+        console.log(err, res[0].hash);
     })
   }
+
+  // instantiateContract() {
+  //   /*
+  //    * SMART CONTRACT EXAMPLE
+  //    *
+  //    * Normally these functions would be called in the context of a
+  //    * state management library, but for convenience I've placed them here.
+  //    */
+
+  //   const contract = require('truffle-contract')
+  //   const simpleStorage = contract(SimpleStorageContract)
+  //   simpleStorage.setProvider(this.state.web3.currentProvider)
+
+  //   // Declaring this for later so we can chain functions on SimpleStorage.
+  //   var simpleStorageInstance
+
+  //   // Get accounts.
+  //   this.state.web3.eth.getAccounts((error, accounts) => {
+  //     simpleStorage.deployed().then((instance) => {
+  //       simpleStorageInstance = instance
+
+  //       // Stores a given value, 5 by default.
+  //       return simpleStorageInstance.set(5, {from: accounts[0]})
+  //     }).then((result) => {
+  //       // Get the value from the contract to prove it worked.
+  //       return simpleStorageInstance.get.call(accounts[0])
+  //     }).then((result) => {
+  //       // Update state with the result.
+  //       return this.setState({ storageValue: result.c[0] })
+  //     })
+  //   })
+  // }
 
   render() {
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-            </div>
-          </div>
-        </main>
+        <form onSubmit={this.upload}>
+          <input type="file" onChange={this.captureFile}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
